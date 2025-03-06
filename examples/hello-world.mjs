6464c675709a853a6110a26c30bd2ac3b5447164
よね/Yone <git@yoneyo.com>
2025-03-06 22:37:04 +0900

    update(examples): set comment in script

--- examples/hello-world.mjs
+++ examples/hello-world.mjs
@@ -1,3 +1,14 @@
+/**!
+ * 
+ * render.js
+ * 
+ * Copyright (C) 2025 よね/Yone
+ * 
+ * Licensed under the MIT License.
+ * 
+ * https://github.com/yone1130/render.js
+ * 
+ */
 
 import { Render } from 'https://cdn.yoneyo.com/scripts/render-v1.0.0.mjs';
 
