65cb2d4d37ae995088ac9e9fc3802ded9aa22846
よね/Yone <git@yoneyo.com>
2025-12-24 13:35:23 +0900

    Test(app-creator): Add new test for `app-creator`

---
+++ tests/app-creator/scripts/app-creator.js
@@ -0,0 +1,53 @@
+/**!
+ * 
+ * render.js
+ * 
+ * Copyright (C) 2025 よね/Yone
+ * 
+ * Licensed under the MIT License.
+ * 
+ * https://github.com/yone1130/render.js
+ * 
+ */
+
+import { App } from "./features/app/app.js";
+
+class TestAppCreator {
+    constructor({ root }) {
+        this.root = root;
+    }
+
+    async main() {
+        const { Render, RenderApp, RenderComponent } = await this.#importRender();
+        this.render = new Render();
+        this.rendering();
+    }
+
+    rendering() {
+        this.render.runApp({
+            root: this.root,
+            app: new App(),
+        })
+    }
+
+    async #importRender() {
+        try {
+            return await import('../../../dist/render.js');
+        } catch (error) {
+            this.#onFailedImportRender(error);
+        }
+    }
+
+    #onFailedImportRender(error) {
+        root.textContent = "Error: Failed to import the render.js package. Please build the project first.";
+        throw new Error(`Error: Failed to import the render.js package. Please build the project first.: ${error}`);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const testAppCreator = new TestAppCreator({
+        root: document.getElementById("root"),
+    });
+
+    testAppCreator.main();
+});
---
+++ tests/app-creator/scripts/features/app/app.js
@@ -0,0 +1,28 @@
+/**!
+ * 
+ * render.js
+ * 
+ * Copyright (C) 2025 よね/Yone
+ * 
+ * Licensed under the MIT License.
+ * 
+ * https://github.com/yone1130/render.js
+ * 
+ */
+
+import { RenderApp } from '../../../../../dist/render.js';
+import { HelloWorld } from './components/hello-world.js';
+
+export class App extends RenderApp {
+    constructor() {
+        super();
+    }
+
+    build() {
+        return super.build({
+            children: [
+                new HelloWorld(),
+            ]
+        });
+    }
+}
